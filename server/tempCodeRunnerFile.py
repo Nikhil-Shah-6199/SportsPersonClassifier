fa2bafa2f59a62a9710831cd82aac6e5331b7c9b
Nikhil Shah <97539685+Nikhil-Shah-6199@users.noreply.github.com>
2024-08-09 15:32:57 +0530

    commit changes

---
+++ server/tempCodeRunnerFile.py
@@ -0,0 +1,4 @@
+    # print(classify_image(None, "./test_images/federer1.jpg"))
+    # print(classify_image(None, "./test_images/federer2.jpg"))
+    # print(classify_image(None, "./test_images/virat1.jpg"))
+    # print(classify_image(None, "./test_images/virat2.jpg"))
